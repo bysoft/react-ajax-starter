891c3091b931849af6088c9ced96b00821cbe97d
Brian Yang <brianyang@users.noreply.github.com>
2016-09-29 00:04:35 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import axios from 'axios';
+import React, { Component } from 'react'
+import logo from './logo.svg'
+import './App.css'
+import axios from 'axios'
 import json from './properties.json'
 
 class App extends Component {
@@ -20,28 +20,20 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-
-      <ul>
+      <div className="App">             
+        <ul>
           {this.state.results.map(result =>
-              <li key={result.id}>
-                  <div>
-                      <h1>{ result.title }</h1>
-                      <p>{ result.description }</p>
-                  </div>
-              </li>
+            <li key={result.id}>
+              <div>
+                <h1>{ result.title }</h1>
+                <p>{ result.description }</p>
+              </div>
+            </li>
           )}
       </ul>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
